1455d3a3dd317f94229f7314506c1c49a844c8e0
wsppt <zmilk2013@126.com>
2022-05-19 15:34:46 +0800

    Update notice.go

--- notice.go
+++ notice.go
@@ -11,6 +11,7 @@ type AndroidNotice struct {
 	Alert     string                 `json:"alert"`
 	Title     string                 `json:"title,omitempty"`
 	BuilderId int                    `json:"builder_id,omitempty"`
+	Intent    map[string]string      `json:"intent,omitempty"`
 	Extras    map[string]interface{} `json:"extras,omitempty"`
 }
 
